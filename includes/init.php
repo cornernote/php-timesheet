39db0adf1eb08757933235630771719c3a722733
cornernote <cornernote@gmail.com>
2014-04-07 11:35:11 +0930

    added kint for debugging

--- includes/init.php
+++ includes/init.php
@@ -1,7 +1,7 @@
 <?php
 $_ENV['config'] = require(dirname(__FILE__) . '/config.php');
 require(dirname(__FILE__) . '/globals.php');
-//require(bp().'/vendors/krumo/krumo_0.2.1a/class.krumo.php');
+require(bp() . '/vendors/kint/Kint.class.php');
 
 error_reporting(E_ALL);
 date_default_timezone_set('Australia/Adelaide');
