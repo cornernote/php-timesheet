0c155d17040edf47e7296cc94187863716d1af97
cornernote <cornernote@gmail.com>
2014-06-02 11:09:03 +0930

    added Kint::trace() to better view errors

--- classes/Base.php
+++ classes/Base.php
@@ -1,7 +1,14 @@
 <?php
+
+/**
+ * Class Base
+ */
 abstract class Base
 {
-    public $ignoreProperties = array (
+    /**
+     * @var array
+     */
+    public $ignoreProperties = array(
         'searchTerm',
         'sortColumn',
         'sortOrder',
@@ -10,17 +17,22 @@ abstract class Base
         'timeSortOrder',
         'dueAlerted',
         'estimateAlerted',
-);
+    );
+
+    /**
+     * @param array $config
+     */
     public function __construct($config = array())
     {
         if (isset($config)) {
             $properties = get_object_vars($this);
             foreach ($config as $key => $value) {
-                if (!array_key_exists($key,$properties)) {
-                    if (in_array($key ,$this->ignoreProperties)){
+                if (!array_key_exists($key, $properties)) {
+                    if (in_array($key, $this->ignoreProperties)) {
                         continue;
                     }
-                    echo "<br/> missing property [" . get_class($this) . '/' . $key . "]  File:" . __FILE__ . " line:" . __LINE__ . "<br/>\r\n";
+                    echo '<br/>missing property [' . get_class($this) . '/' . $key . ']';
+                    Kint::trace();
                 }
                 $this->$key = $value;
             }
